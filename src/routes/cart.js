7575ead47ff16fd16edbe8eccde3720f63b5e412
jcantini <jorge.cantinigmail.com>
2021-10-01 17:03:53 -0300

    Agregado de una ruta adicional de ordenes para el administrador

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,6 +5,45 @@ const { Cart } = require('../db')
 
 const router = Router();
 
+//Muestra todas las ordenes para el administrador
+router.get('/admin', async(req, res) => {
+    try {
+      const result = await Cart.findAll({
+          attributes: ['state','payment_method','createdAt'],    
+          include: [
+           { model: User,
+             attributes: ['name','lastname']},
+           { model: Product,
+             attributes: ['name','price'],
+             through: {
+                 attributes: ['quantity']
+             }
+           }] 
+      }); 
+      if (!result.length) return res.status(400).send('No orders found')
+      return res.json(result)
+      const order= {}
+      const date = new Date(result[0].createdAt)
+      const dia = date.getDate() + '/' + (date.getMonth()+1)  + '/' + date.getFullYear()
+      order.uname = result[0].user.name
+      order.ulastname = result[0].user.lastname
+      order.state = result[0].state
+      order.payment_method = result[0].payment_method
+      order.created = dia
+      order.products = result[0].products.map( p => {
+                return {
+                  name : p.name,
+                  price : p.price,
+                  quantity : p.products_carts.quantity
+                }
+              })
+        
+      res.status(200).json(order) 
+    } catch (error) {
+        res.send('Error: ', error)
+    }
+ });
+
 // Muestra una orden para un Id
 router.get('/:id', async(req, res) => {
    const { id } = req.params
