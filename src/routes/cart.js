9bcf0633f4c1c0f99e00b1fcb74bbbc226800202
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-01 19:29:16 -0500

    Edit cart with add, remove, removeproduct and addproduct

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -20,49 +20,59 @@ router.put('/', async (req, res) => {
               if (aux) {
                 aux.quantity = aux.quantity + 1;
               }
-              aux.save()
+              await aux.save()
             }
-
+            await cartdb.save();
           }
-          // await cartdb.products.forEach(async(product) => {
-          //   if (product.id === idProduct) {
-          //     const aux = await Products_carts.findOne({where: {productId: idProduct, cartId }})
-          //     if(aux){
-          //       aux.quantity = aux.quantity + 1;
-          //     }
-          //     aux.save()
-          //   }
-          // });
         }
         if (action === "remove") {
-          await cartdb.products.forEach(async (product) => {
-            if (product.id === idProduct) {
-              if ((product.products_carts.quantity - 1) <= 0) {
-                const deleteProduct = await Products_carts.destroy({ where: { cartId, productId: product.id } });
+          for (let i = 0; i < cartdb.products.length; i++) {
+            if (cartdb.products[i].id === idProduct) {
+              if ((cartdb.products[i].products_carts.quantity - 1) <= 0) {
+                const deleteProduct = await Products_carts.destroy({ where: { cartId, productId: cartdb.products[i].id } });
               } else {
                 const aux = await Products_carts.findOne({ where: { productId: idProduct, cartId } })
                 if (aux) {
                   aux.quantity = aux.quantity - 1;
                 }
-                aux.save()
+                await aux.save();
               }
             }
-          });
+            await cartdb.save();
+          }
+          // await cartdb.products.forEach(async (product) => {
+          //   if (product.id === idProduct) {
+          //     if ((product.products_carts.quantity - 1) <= 0) {
+          //       const deleteProduct = await Products_carts.destroy({ where: { cartId, productId: product.id } });
+          //     } else {
+          //       const aux = await Products_carts.findOne({ where: { productId: idProduct, cartId } })
+          //       if (aux) {
+          //         aux.quantity = aux.quantity - 1;
+          //       }
+          //       aux.save()
+          //     }
+          //   }
+          //   cartdb.save();
+          // });
         }
         if (action === "addproduct") {
           const productdb = await Product.findByPk(idProduct)
           if (productdb) {
-            await cartdb.setProducts([productdb], { through: { quantity: 1 } })
+            //user.addProject(project, { through: { role: 'manager' }});
+            await cartdb.addProduct(productdb, { through: { quantity: 1 } })
+            await cartdb.save();
           } else {
             return res.status(404).json({ msg: "El producto que intenta agregar no existe" })
           }
         }
         if (action === "removeproduct") {
           const deleteProduct = await Products_carts.destroy({ where: { cartId, productId: idProduct } });
+          cartdb.save();
         }
 
-        cartdb.save();
-        return res.json(cartdb)
+        await cartdb.save();
+        const aux = await Cart.findOne({ where: { id: cartId }, include: Product })
+        return res.json(aux)
       } else {
         return res.status(404).json({ msg: `No existe ningún carrito con este id:${cartId}` })
       }
@@ -159,131 +169,140 @@ router.get('/:id', async (req, res) => {
 });
 
 //Muestra todas las ordenes para el administrador
-router.get('/admin', async(req, res) => {
-    try {
-      const result = await Cart.findAll({
-          attributes: ['state','payment_method','createdAt'],    
-          include: [
-           { model: User,
-             attributes: ['name','lastname']},
-           { model: Product,
-             attributes: ['name','price'],
-             through: {
-                 attributes: ['quantity']
-             }
-           }] 
-      }); 
-      if (!result.length) return res.status(400).send('No orders found')
-      return res.json(result)
-      const order= {}
-      const date = new Date(result[0].createdAt)
-      const dia = date.getDate() + '/' + (date.getMonth()+1)  + '/' + date.getFullYear()
-      order.uname = result[0].user.name
-      order.ulastname = result[0].user.lastname
-      order.state = result[0].state
-      order.payment_method = result[0].payment_method
-      order.created = dia
-      order.products = result[0].products.map( p => {
-                return {
-                  name : p.name,
-                  price : p.price,
-                  quantity : p.products_carts.quantity
-                }
-              })
-        
-      res.status(200).json(order) 
-    } catch (error) {
-        res.send('Error: ', error)
-    }
- });
+router.get('/admin', async (req, res) => {
+  try {
+    const result = await Cart.findAll({
+      attributes: ['state', 'payment_method', 'createdAt'],
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'lastname']
+        },
+        {
+          model: Product,
+          attributes: ['name', 'price'],
+          through: {
+            attributes: ['quantity']
+          }
+        }]
+    });
+    if (!result.length) return res.status(400).send('No orders found')
+    return res.json(result)
+    const order = {}
+    const date = new Date(result[0].createdAt)
+    const dia = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+    order.uname = result[0].user.name
+    order.ulastname = result[0].user.lastname
+    order.state = result[0].state
+    order.payment_method = result[0].payment_method
+    order.created = dia
+    order.products = result[0].products.map(p => {
+      return {
+        name: p.name,
+        price: p.price,
+        quantity: p.products_carts.quantity
+      }
+    })
+
+    res.status(200).json(order)
+  } catch (error) {
+    res.send('Error: ', error)
+  }
+});
 
 // Muestra una orden para un Id
-router.get('/:id', async(req, res) => {
-   const { id } = req.params
-   try {
-     const result = await Cart.findAll({
-         where : {userId: id},
-         attributes: ['state','payment_method','createdAt', 'orderid', 'paymentStatus'],    
-         include: [
-          { model: User,
-            attributes: ['name','lastname']},
-          { model: Product,
-            attributes: ['name','price'],
-            through: {
-                attributes: ['quantity']
-            }
-          }] 
-     }); 
-     const order= {}
-     const date = new Date(result[0].createdAt)
-     const dia = date.getDate() + '/' + (date.getMonth()+1)  + '/' + date.getFullYear()
-     order.uname = result[0].user.name
-     order.ulastname = result[0].user.lastname
-     order.orderid = result[0].orderid
-     order.paymentStatus = result[0].paymentStatus
-     order.state = result[0].state
-     order.payment_method = result[0].payment_method
-     order.created = dia
-     order.products = result[0].products.map( p => {
-               return {
-                 name : p.name,
-                 price : p.price,
-                 quantity : p.products_carts.quantity
-               }
-             })
-     res.status(200).json(order) 
-   } catch (error) {
-       res.send('Error: ', error)
-   }
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const result = await Cart.findAll({
+      where: { userId: id },
+      attributes: ['state', 'payment_method', 'createdAt', 'orderid', 'paymentStatus'],
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'lastname']
+        },
+        {
+          model: Product,
+          attributes: ['name', 'price'],
+          through: {
+            attributes: ['quantity']
+          }
+        }]
+    });
+    const order = {}
+    const date = new Date(result[0].createdAt)
+    const dia = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+    order.uname = result[0].user.name
+    order.ulastname = result[0].user.lastname
+    order.orderid = result[0].orderid
+    order.paymentStatus = result[0].paymentStatus
+    order.state = result[0].state
+    order.payment_method = result[0].payment_method
+    order.created = dia
+    order.products = result[0].products.map(p => {
+      return {
+        name: p.name,
+        price: p.price,
+        quantity: p.products_carts.quantity
+      }
+    })
+    res.status(200).json(order)
+  } catch (error) {
+    res.send('Error: ', error)
+  }
 });
 
 
 // Muestra una ordenes para un usuario
-router.get('/', async(req, res) => {
-    const { userid, state } = req.query
-    let condition = {}
-    if (state === 'activas') {
-        condition.userId = userid
-        condition.state = 'in process'
-    } else {
-        condition.userId = userid
-        condition.state = ['cancelled', 'finished']
-    }
-    try {
-      const result = await Cart.findAll({
-          where : condition,
-          attributes: ['state','payment_method','createdAt'],    
-          include: [
-           { model: User,
-             attributes: ['name','lastname']},
-           { model: Product,
-             attributes: ['name','price'],
-             through: {
-                 attributes: ['quantity']
-             }
-           }] 
-      }); 
-      if (!result.length) return res.status(400).send('No orders found')
-      const order= {}
-      const date = new Date(result[0].createdAt)
-      const dia = date.getDate() + '/' + (date.getMonth()+1)  + '/' + date.getFullYear()
-      order.uname = result[0].user.name
-      order.ulastname = result[0].user.lastname
-      order.state = result[0].state
-      order.payment_method = result[0].payment_method
-      order.created = dia
-      order.products = result[0].products.map( p => {
-                return {
-                  name : p.name,
-                  price : p.price,
-                  quantity : p.products_carts.quantity
-                }
-              })
-        
-      res.status(200).json(order) 
-    } catch (error) {
-        res.send('Error: ', error)
-    }
- });
+router.get('/', async (req, res) => {
+  const { userid, state } = req.query
+  let condition = {}
+  if (state === 'activas') {
+    condition.userId = userid
+    condition.state = 'in process'
+  } else {
+    condition.userId = userid
+    condition.state = ['cancelled', 'finished']
+  }
+  try {
+    const result = await Cart.findAll({
+      where: condition,
+      attributes: ['state', 'payment_method', 'createdAt'],
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'lastname']
+        },
+        {
+          model: Product,
+          attributes: ['name', 'price'],
+          through: {
+            attributes: ['quantity']
+          }
+        }]
+    });
+    if (!result.length) return res.status(400).send('No orders found')
+    const order = {}
+    const date = new Date(result[0].createdAt)
+    const dia = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+    order.uname = result[0].user.name
+    order.ulastname = result[0].user.lastname
+    order.state = result[0].state
+    order.payment_method = result[0].payment_method
+    order.created = dia
+    order.products = result[0].products.map(p => {
+      return {
+        name: p.name,
+        price: p.price,
+        quantity: p.products_carts.quantity
+      }
+    })
+
+    res.status(200).json(order)
+  } catch (error) {
+    res.send('Error: ', error)
+  }
+});
 
 module.exports = router;
