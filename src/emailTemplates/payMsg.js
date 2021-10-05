81cefc8d69ef4a58611f64db50473c49a81ff717
unknown <fragusan2@gmail.com>
2021-10-05 11:46:40 -0300

    Se envia mail al user al ejecutar las pasarelas

---
+++ src/emailTemplates/payMsg.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+    Msuccess1: `¡En buena hora 😁! Te escribimos para informarte que tu compra 🛒 N°`,
+    Msuccess2: `, se completo de manera satisfactoria. Esperamos lo disfrutes. Podes descargar la facturación correpondiente, entérarte de nuestras ultimas promociones y nuevos productos en 👉🏻 https://infallible-boyd-567028.netlify.app/ 👈🏻`,
+
+    Mfailure1: `Algo ha marchado mal. Te escribimos para informarte que el pago de tu compra N°`,
+    Mfailure2: `, no pudo completarse. Si la has cancelado tú, puedes entérarte de nuestras ultimas promociones y productos en ofertas en 👉🏻 https://infallible-boyd-567028.netlify.app/ 👈🏻. Si no fuiste tú y aún deseas efectuar la compra, tal vez desees corroborar los datos de tus credenciales de pago ingresada o probar con un medio diferente, nosotros encantados de volverte a ver en nuestra web 😁. ¡Que tengas un buen día!`,
+    //body.client
+    Psuccess1: `¡En buena hora 😁! Te escribimos para informarte que tu compra 🛒 N°`,
+    Psuccess2: `, se completo de manera satisfactoria. Esperamos lo disfrutes. Podes descargar la facturación correpondiente, entérarte de nuestras ultimas promociones y nuevos productos en 👉🏻 https://infallible-boyd-567028.netlify.app/ 👈🏻`,
+
+    Pfailure1: `Algo ha marchado mal. Te escribimos para informarte que el pago de tu compra N°`,
+    Pfailure2: `, no pudo completarse. Si la has cancelado tú, puedes entérarte de nuestras ultimas promociones y productos en ofertas en 👉🏻 https://infallible-boyd-567028.netlify.app/ 👈🏻. Si no fuiste tú y aún deseas efectuar la compra, tal vez desees corroborar los datos de tus credenciales de pago ingresada o probar con un medio diferente, nosotros encantados de volverte a ver en nuestra web 😁. ¡Que tengas un buen día!`
+
+}
