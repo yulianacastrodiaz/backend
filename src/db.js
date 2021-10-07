4cc8941620c537cec1815ad87be70cd6b1c271d3
jcantini <jorge.cantinigmail.com>
2021-10-07 07:57:21 -0300

    Agregado de Wishlist

--- src/db.js
+++ src/db.js
@@ -37,7 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-const { Category, SubCategory, Grape, Product, User,  Review, Cart, Products_carts } = sequelize.models;
+const { Category, SubCategory, Grape, Product, User,  Review, Cart, Products_carts, Wishlist, wlist_prods} = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
@@ -57,6 +57,11 @@ User.hasMany(Cart)
 Cart.belongsTo(User)
 Product.belongsToMany(Cart, { through: Products_carts })
 Cart.belongsToMany(Product, { through: Products_carts })
+User.hasOne(Wishlist);
+Wishlist.belongsTo(User)
+Product.belongsToMany(Wishlist, { through: 'wlist_prods' })
+Wishlist.belongsToMany(Product, { through: 'wlist_prods' })
+
 // User.hasMany(User) 
 
 module.exports = {
